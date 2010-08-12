f2bab47d93746ca41aa303dbcee363ec9e9d020d
Gal Steinitz <gal@steinitz.com>
2010-08-11 18:33:04 -0700

    adds is_apple?, is_ie? and adds facebooks bot's user agent

--- lib/simple-useragent.rb
+++ lib/simple-useragent.rb
@@ -4,7 +4,7 @@ class SimpleUserAgent
   # Some mobile browsers which look like desktop browsers.
   MOBILE = /(iphone|ipod|blackberry|android|palm|windows\s+ce)/i
   DESKTOP = /(windows|linux|os\s+[x9]|solaris|bsd)/i
-  BOT = /(spider|crawl|slurp|bot)/i
+  BOT = /(spider|crawl|slurp|bot|facebookexternalhit)/i
 
   #Anything that looks like a phone isn't a desktop.
   #Anything that looks like a desktop probably is.
@@ -27,6 +27,16 @@ class SimpleUserAgent
     user_agent = get_user_agent(request_or_user_agent)
     !!(user_agent =~ /BlackBerry/)
   end
+  
+  def self.is_apple?(request_or_user_agent)
+    user_agent = get_user_agent(request_or_user_agent)
+    !!(user_agent =~ /(Mobile\/.+Safari)/) || !!(user_agent =~ /OS\s+[X9]/)
+  end
+  
+  def self.is_ie?(request_or_user_agent)
+    user_agent = get_user_agent(request_or_user_agent)
+    !!(user_agent =~ /MSIE/)
+  end
 
   # Some mobile browsers put the User-Agent in a HTTP-X header
   def self.get_user_agent(request_or_user_agent)
@@ -44,5 +54,12 @@ class SimpleUserAgent
     return 'ie8' if user_agent =~ /MSIE 8/
     return 'ie7' if user_agent =~ /MSIE 7/
     return 'ie6' if user_agent =~ /MSIE 6/
+  end
+  
+  def self.os(request_or_user_agent)
+    user_agent = get_user_agent(request_or_user_agent)
+    return 'mac' if user_agent =~ /macintosh/i
+    return 'windows' if user_agent =~ /windows/i
+    return 'linux' if user_agent =~ /linux|solaris|bsd/i
   end
 end
